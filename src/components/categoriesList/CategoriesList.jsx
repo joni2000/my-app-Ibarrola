3bd943a3a65bc4722b8c95d4d1e53737be9a8c7a
Jonathan Ibarrola <ibarrolajonathan@gmail.com>
2022-09-12 20:24:09 -0300

    agregando menu desplegable de categorias

---
+++ src/components/categoriesList/CategoriesList.jsx
@@ -0,0 +1,31 @@
+import { Box, List, ListItem, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+
+export const CategoriesList = ({ categories }) => {
+
+    console.log(categories)
+
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        maxWidth: 200,
+        bgcolor: "black",
+        position: "absolute",
+        top: 23,
+        left: 4,
+        borderRadius: 1
+      }}
+    >
+      <List>
+        { categories.map(({ id, name }) => (
+          <ListItem disablePadding sx={{ textAlign: "center" }} key={ id }>
+            <Link to={`/category/${ id }`}>
+              <Typography sx={{ fontSize: { sm: 15 }, color: '#ffffff'}}>{ name }</Typography>
+            </Link>
+          </ListItem>
+        ) )}
+      </List>
+    </Box>
+  );
+};
